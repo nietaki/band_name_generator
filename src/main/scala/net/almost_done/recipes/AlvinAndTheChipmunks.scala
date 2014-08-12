0a8d373a1c5c1085dc14daba79c01b13ec6ea131
Jacek Królikowski <nietaki@gmail.com>
2014-08-12 14:17:39 +0200

    3 recipes done

--- src/main/scala/net/almost_done/recipes/AlvinAndTheChipmunks.scala
+++ src/main/scala/net/almost_done/recipes/AlvinAndTheChipmunks.scala
@@ -1,8 +1,14 @@
 package net.almost_done.recipes
 
+import net.almost_done._
+
 /**
  * Created by nietaki on 12.08.14.
  */
-object AlvinAndTheChipmunks {
+object AlvinAndTheChipmunks extends BandNameRecipe {
+  override def requirements: List[SpeechPart] = Noun :: Plural :: Nil
 
+  override def stitcher: PartialFunction[List[Word], String] =  {
+    case noun :: plural :: Nil => s"${noun.capitalized.word} and the ${plural.capitalized.word}"
+  }
 }
---
+++ src/main/scala/net/almost_done/recipes/GreenDay.scala
@@ -0,0 +1,13 @@
+package net.almost_done.recipes
+
+import net.almost_done._
+/**
+ * Created by nietaki on 12.08.14.
+ */
+object GreenDay extends BandNameRecipe{
+  override def requirements: List[SpeechPart] = Adjective :: NounLike :: Nil
+
+  override def stitcher: PartialFunction[List[Word], String] = {
+    case adj :: nounLike :: Nil => s"${adj.capitalized.word} ${nounLike.capitalized.word}"
+  }
+}
