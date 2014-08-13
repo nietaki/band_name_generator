b4bbaffa86296d4d32289959a165c10e2e88f809
Jacek Królikowski <nietaki@gmail.com>
2014-08-13 15:57:40 +0200

    the form partially works

--- public/javascripts/bng.js
+++ public/javascripts/bng.js
@@ -6,11 +6,37 @@
   console.log('bng.js')
   var app = angular.module('bng', [])
 
-  app.controller('BandNameController', function($scope) {
-    console.log('bandnamecontroller')
-      $scope.processForm = function() {
-        console.log('processForm')
+  app.controller('BandNameController', function($scope, $http) {
+    $scope.formData = {}
+    $scope.formData.recipes = [
+      {
+        name: 'GreenDay',
+        example: 'Green Day',
+        selected: true
+      },
+      {
+        name: 'TheWhiteStripes',
+        example: 'The White Stripes',
+        selected: false
       }
+    ]
+    $scope.uncommonProbability = 0.1
+    $scope.nameCount = 20
+    console.log('bandnamecontroller')
+
+    $scope.bandNames=["foo"]
+
+    $scope.processForm = function() {
+      console.log('processForm')
+      var addr = '/bandNames/' +  $scope.uncommonProbability + '/' + $scope.nameCount
+      $http.post(addr, $scope.formData)
+        .success(function(data){
+          console.log('success')
+          console.log(data)
+          $scope.bandNames = data
+        })
+        .error(function(data){console.log('error'); console.log(data)})
+    }
   });
 
 })();
